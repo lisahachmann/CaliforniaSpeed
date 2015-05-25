538beeca5d676588577188baae9e340d241df4d0
li <lisa.hachmann@gmail.com>
2015-05-25 15:52:56 -0400

    First card

--- singleplayergame.py
+++ singleplayergame.py
@@ -1,6 +1,12 @@
 import pygame
 pygame.init()
 
+#constants
+WHITE = (255, 255 , 255)
+GREEN = (0, 200, 100)
+RED = (255, 0, 0)
+BLACK = (0,0,0)
+
 class Window():
 	size = (700, 500)
 	screen = pygame.display.set_mode(size)
@@ -8,27 +14,29 @@ class Window():
 
 class Card(object):
 	"""Every card is an object of the Card class"""
+	size = (70, 50)
 	def __init__(self):
 		pass
 
-	def make_hearts():
+	def make_hearts(self):
 		pass
-	def make_spades():
+	def make_spades(self):
 		pass
-	def make_diamonds():
+	def make_diamonds(self):
 		pass
-	def make_clubs():
+	def make_clubs(self):
 		pass
+	def back_of_card(self):
+		pygame.draw.rect(Window.screen, RED, (20,20,50,70), 0) 
+		#screen, color, (x, y, width, height), thickness
 
 def deal_cards():
 	pass
 
 
 Game = Window()
-WHITE = (255, 255 , 255)
-GREEN = (0, 200, 100)
-RED = (255, 0, 0)
-BLACK = (0,0,0)
+
+
 done = False
 clock = pygame.time.Clock()
 
@@ -39,13 +47,16 @@ while not done:
  		elif event.type == pygame.MOUSEBUTTONDOWN:
  			print "User pressed the mouse button"
     # --- Game logic should go here
-    
-    # --- Drawing code should go here
- 
+
+
+
     # First, clear the screen to green. Don't put other drawing commands
     # above this, or they will be erased with this command.
 	Window.screen.fill(GREEN)
- 
+	# --- Drawing code should go here
+	First_Card = Card()
+	First_Card.back_of_card() 
+ 	pygame.display.update()
     # --- Go ahead and update the screen with what we've drawn.
 	pygame.display.flip()
 	clock.tick(60)
